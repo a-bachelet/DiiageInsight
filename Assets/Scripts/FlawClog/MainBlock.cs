807267e694a0146257ac5038e45d0c602b248241
Alexis Bachelet <alexis.bachelet@diiage.org>
2020-03-01 21:12:07 +0100

    Started working on main block behavior

---
+++ Assets/Scripts/FlawClog/MainBlock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBlock : MonoBehaviour
+{
+    public List<Material> materials = new List<Material>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("changeBlockMaterial", 1f, 0.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void changeBlockMaterial()
+    {
+        int rnd = Random.Range(0, materials.ToArray().Length);
+
+        GetComponent<Renderer>().material = materials[rnd];       
+    }
+}
